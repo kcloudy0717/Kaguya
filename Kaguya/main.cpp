2e94ffccc3b5b1f7efd63016a89b9b4cc0652a32
Kai <kcloudy0717@gmail.com>
2021-08-15 17:51:13 -0400

    No longer need to rebuild when shaders are modified

--- Kaguya/main.cpp
+++ Kaguya/main.cpp
@@ -56,10 +56,8 @@ public:
 		VERIFY_VULKAN_API(vkBeginCommandBuffer(cmd, &CommandBufferBeginInfo));
 
 		// make a clear-color from frame number. This will flash with a 120*pi frame period.
-		VkClearValue	clearValue;
-		static uint32_t _frameNumber = 0;
-		float			flash		 = abs(sin(_frameNumber / 120.f));
-		clearValue.color			 = { { 0.0f, 0.0f, flash, 1.0f } };
+		VkClearValue clearValue;
+		clearValue.color = { { 0.0f, 0.0f, 0.0f, 1.0f } };
 
 		// start the main renderpass.
 		// We will use the clear color from above, and the framebuffer of the index the swapchain gave us
@@ -109,9 +107,6 @@ public:
 		VERIFY_VULKAN_API(vkQueueSubmit(Device.GetGraphicsQueue().GetAPIHandle(), 1, &SubmitInfo, RenderFence));
 
 		SwapChain.Present(RenderSemaphore);
-
-		// increase the number of frames drawn
-		_frameNumber++;
 	}
 
 	void Shutdown() override
@@ -343,3 +338,136 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+
+#if 0
+// main.cpp : Defines the entry point for the application.
+//
+	#include <Core/Application.h>
+	#include <Physics/PhysicsManager.h>
+	#include <RenderCore/RenderCore.h>
+
+	#include <Graphics/AssetManager.h>
+	#include <Graphics/UI/HierarchyWindow.h>
+	#include <Graphics/UI/ViewportWindow.h>
+	#include <Graphics/UI/InspectorWindow.h>
+	#include <Graphics/UI/AssetWindow.h>
+	#include <Graphics/UI/ConsoleWindow.h>
+
+	#include <Graphics/Renderer.h>
+	#include <Graphics/PathIntegrator.h>
+
+	#define RENDER_AT_1920x1080 0
+
+class Editor : public Application
+{
+public:
+	Editor() {}
+
+	~Editor() {}
+
+	bool Initialize() override
+	{
+		PhysicsManager::Initialize();
+		RenderCore::Initialize();
+		AssetManager::Initialize();
+
+		std::string iniFile = (Application::ExecutableDirectory / "imgui.ini").string();
+
+		ImGui::LoadIniSettingsFromDisk(iniFile.data());
+
+		HierarchyWindow.SetContext(&World);
+		InspectorWindow.SetContext(&World, {});
+		AssetWindow.SetContext(&World);
+
+		pRenderer = std::make_unique<PathIntegrator>(&World);
+		pRenderer->OnInitialize();
+
+		return true;
+	}
+
+	void Update(float DeltaTime) override
+	{
+		World.WorldState = EWorldState::EWorldState_Render;
+
+		ImGui_ImplDX12_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+		ImGuizmo::BeginFrame();
+
+		ImGui::DockSpaceOverViewport();
+
+		ImGui::ShowDemoWindow();
+
+		ImGuizmo::AllowAxisFlip(false);
+
+		ViewportWindow.SetContext(pRenderer->GetViewportDescriptor());
+		// Update selected entity here in case Clear is called on HierarchyWindow to ensure entity is invalidated
+		InspectorWindow.SetContext(&World, HierarchyWindow.GetSelectedEntity());
+
+		HierarchyWindow.Render();
+		ViewportWindow.Render();
+		AssetWindow.Render();
+		ConsoleWindow.Render();
+		InspectorWindow.Render();
+
+	#if RENDER_AT_1920x1080
+		const uint32_t viewportWidth = 1920, viewportHeight = 1080;
+	#else
+		const uint32_t viewportWidth = ViewportWindow.Resolution.x, viewportHeight = ViewportWindow.Resolution.y;
+	#endif
+
+		World.Update(DeltaTime);
+
+		// Render
+		pRenderer->OnSetViewportResolution(viewportWidth, viewportHeight);
+
+		pRenderer->OnRender();
+	}
+
+	void Shutdown() override
+	{
+		pRenderer->OnDestroy();
+		pRenderer.reset();
+		World.Clear();
+		AssetManager::Shutdown();
+		RenderCore::Shutdown();
+		PhysicsManager::Shutdown();
+	}
+
+	void Resize(UINT Width, UINT Height) override { pRenderer->OnResize(Width, Height); }
+
+private:
+	HierarchyWindow HierarchyWindow;
+	ViewportWindow	ViewportWindow;
+	InspectorWindow InspectorWindow;
+	AssetWindow		AssetWindow;
+	ConsoleWindow	ConsoleWindow;
+
+	World					  World;
+	std::unique_ptr<Renderer> pRenderer;
+};
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		Application::InitializeComponents("Kaguya");
+
+		const ApplicationOptions AppOptions = { .Name	  = L"Kaguya",
+												.Width	  = 1280,
+												.Height	  = 720,
+												.Maximize = true,
+												.Icon	  = Application::ExecutableDirectory / "Assets/Kaguya.ico" };
+
+		Editor App;
+		Application::Run(App, AppOptions);
+	}
+	catch (std::exception& Exception)
+	{
+		MessageBoxA(nullptr, Exception.what(), "Error", MB_OK | MB_ICONERROR | MB_DEFAULT_DESKTOP_ONLY);
+		return 1;
+	}
+
+	return 0;
+}
+#endif
--- Shaders/Vulkan/Triangle.vs.hlsl
+++ Shaders/Vulkan/Triangle.vs.hlsl
@@ -7,7 +7,7 @@ struct VSOutput
 VSOutput main(uint VertexID : SV_VertexID)
 {
 	// const array of positions for the triangle
-	const float3 positions[3] = { float3(1.f, 1.f, 0.0f), float3(-1.f, 1.f, 0.0f), float3(0.f, -1.f, 0.0f) };
+    const float3 positions[3] = { float3(0.5f, 0.5f, 0.0f), float3(-0.5f, 0.5f, 0.0f), float3(0.f, -0.5f, 0.0f) };
 	// const array of colors for the triangle
 	const float3 colors[3] = { float3(1.0f, 0.0f, 0.0f),   // red
 							   float3(0.0f, 1.0f, 0.0f),   // green
